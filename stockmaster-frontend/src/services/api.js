c7c01a2c37e75d3c421b4712352105ece6f8b8ee
fernanddadantasm <fernandamoreira079@gmail.com>
2025-12-02 09:38:21 -0300

    adicionando variavel de ambiente

--- stockmaster-frontend/src/services/api.js
+++ stockmaster-frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // sua API backend
+  baseURL: process.env.HOST_API, // sua API backend
+  timeout: 50000 // tempo limite de 10 segundos
 });
 
 export default api;
